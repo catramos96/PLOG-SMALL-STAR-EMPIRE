ee4390dde281fae1f232205c0e1c934d5e63a5da
catramos96 <catramos96@gmail.com>
2016-11-08 13:17:44 +0000

    Revert "Ajuste"
    This reverts commit 4079deaf5018841f4a4a786f3e60f7e437d179ac.

--- SmallStarEmpireCode.pl
+++ SmallStarEmpireCode.pl
@@ -2,12 +2,36 @@
 ************ SMALL STAR EMPIRE **************
 *********************************************/
 
+:-include('Board.pl').
 :-include('Player.pl').	
 :-include('GameFunctionalities.pl')	.					
 														
 /************************
 *	      GAME			*
 ************************/
+
+clearscreen :- write('\e[2J').
+
+game_settings(Board,P1,P2) :- 	clearscreen,
+								board_settings(Board),
+								loadPlayers(Board,P1,P2).
+	
+make_move(Board,Pi,FinalBoard,Pf) :-	moveShip(Board,Pi,FinalBoard,Pf,1).
+
+make_move(Board,Pi,FinalBoard,Pf) :- 	moveShip(Board,Pi,_,_,0), !,
+										make_move(Board,Pi,FinalBoard,Pf).
+									
+turn(Board,Pi,FinalBoard,Pf) :- 	nl,write('NEW TURN - '), 
+									displayTeamName(Pi),
+									displayBoard(Board),
+									displayPlayerInfo(Pi),
+									make_move(Board,Pi,FinalBoard,Pf).
+								
+play(Board,P1i,P2i,P1f,P2f) :- 	clearscreen,
+								turn(Board,P1i,BoardT,P1t),
+								clearscreen,
+								turn(BoardT,P2i,FinalBoard,P2t),
+								play(FinalBoard,P1t,P2t,P1f,P2f).
 				
 game :- game_settings(Board,P1,P2),
 		play(Board,P1,P2,P1f,P2f).

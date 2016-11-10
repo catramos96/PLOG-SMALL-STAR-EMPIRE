9e51b02ec5e8b72230bb23410dd1932025de45b1
Ines <gomes.inesisabel@gmail.com>
2016-11-10 23:40:44 +0000

    Contagem de pontos acaba.

--- GameFunctionalities.pl
+++ GameFunctionalities.pl
@@ -14,7 +14,7 @@ loadPlayers(Board,P1,P2) :- 	getCell(Board,R1,C1,[6,0,N1]),
 moveShip_settings(Ri,Ci,Rf,Cf) :- 	nl, write('From Row'), read(Ri),
 										write('From Column'),read(Ci),
 										write('To Row'), read(Rf),
-										write('To Column'),read(Cf), nl.
+										write('To Column'),read(Cf), nl .
 										
 addControl(Board,Pi,Rf,Cf,Final,Pf) :- 	hasControl(Board,Rf,Cf,R2), R2 is 0, !,	
 										write('Colony(C) or Trade(T)'),	read(Type),
@@ -22,7 +22,7 @@ addControl(Board,Pi,Rf,Cf,Final,Pf) :- 	hasControl(Board,Rf,Cf,R2), R2 is 0, !,
 										setDominion(Board,Team,Rf,Cf,Type,Final),
 										playerAddControl(Pi,Type,[Rf|Cf],Pf).
 										
-addControl(Board,Pi,_,_,Board,Pi) :- 	nl,write('No Control to add'), nl.	
+addControl(Board,Pi,_,_,Board,Pi) :- 	nl,write('No Control to add'), nl .	
 									
 moveShip(Board,Pi,FinalBoard,Pf,1) :- 	moveShip_settings(Ri,Ci,Rf,Cf),
 										validMove(Board,Pi,Ri,Ci,Rf,Cf,1),!,									
@@ -31,4 +31,4 @@ moveShip(Board,Pi,FinalBoard,Pf,1) :- 	moveShip_settings(Ri,Ci,Rf,Cf),
 										setShip(Tmp2,Ri,Ci,-1,FinalBoard),
 										playerSetShip(Pt,[Ri|Ci],[Rf|Cf],Pf).
 				
-moveShip(Board,P,Board,P,0) :- 	nl,write('Movimento invalido!'), nl.
+moveShip(Board,P,Board,P,0) :- 	nl,write('Movimento invalido!'), nl .
--- SmallStarEmpireCode.pl
+++ SmallStarEmpireCode.pl
@@ -22,22 +22,22 @@ make_move(Board,Pi,FinalBoard,Pf) :- 	moveShip(Board,Pi,_,_,0), !,
 										make_move(Board,Pi,FinalBoard,Pf).
 									
 turn(Board,Pi,FinalBoard,Pf) :- 	nl,write('NEW TURN - '), 
-									/*displayTeamName(Pi),*/
+									displayTeamName(Pi),
 									displayBoard(Board),
-									/*displayPlayerInfo(Pi),*/
-									write("OLA"),nl.
-									/*make_move(Board,Pi,FinalBoard,Pf).*/
+									displayPlayerInfo(Pi),
+make_move(Board,Pi,FinalBoard,Pf).
 									
 play(Board,P1i,P2i,P1f,P2f) :- 	clearscreen,
 								turn(Board,P1i,BoardT,P1t), !,
 								clearscreen,
-								turn(BoardT,P2i,FinalBoard,P2t), !,
+								turn(BoardT,P2i,FinalBoard,P2t), ! ,
+								/*winner(FinalBoard,P1t,P2t) .*/
 								play(FinalBoard,P1t,P2t,P1f,P2f).
 								
 game :- game_settings(Board,P1,P2),
-		play(Board,P1,P2,P1f,P2f,Bf) .
+		play(Board,P1,P2,P1f,P2f) .
 		/*board(4,Board),
-		winner(Board,[1,[],[[2,3],[2,1]],[]],[2,[],[[3,4],[4,3]],[]]).*/
+		winner(Board,[1,[[2,3]],[[1,2],[4,3]],[]],[2,[],[[3,3],[3,4],[4,5],[5,4]],[]]) .*/
 
 	
 winner(Board,P1,P2):- 	playerGetPoints(Board,P1,ListLength1,Points1), 
@@ -52,34 +52,39 @@ playerGetPoints(Board,Player,ListLength,Points) :-  playerTerritory(Player,List)
 	
 playerTerritory(Player, List) :- getListElem(Player,2,Trade), getListElem(Player,3,Colony), append(Colony, Trade, List) .	
 
-/*se accR e ou accB: 1->+1; 2->+4; 3->+7; verificar adjacentes as trades (+1 por cada inimigo adjacente)*/
+/*se accR e ou accB: 1->+1; 2->+4; 3->+7; */
 countPoints(_,[],Points,FinalPoints,AccB, AccR) :- 	getNebulaePoints(Points, AccB, NewPoints),
 													getNebulaePoints(NewPoints, AccR, FinalPoints) .
 
-/*deve percorrer todos as colonia/trades: se 1,2 ou 3 soma; se 4 ou 5 coloca num acumulador*/
-countPoints(Board,[[R|[C|[]]]|Lb], AccPoints, FinalPoints, AccB, AccR) :-   getBoardCell(Board,R,C,[ID|_]), 
-																			getSystemTypePoints(ID,P,AccB,AccR,NewAccB,NewAccR), 
-																			/*getTradePoints(Board,R,C,Team,1,AccPoints),*/
-																			NewPoints is AccPoints + P , 
+/*deve percorrer todos as colonia/trades: se 1,2 ou 3 soma; se 4 ou 5 coloca num acumulador ; 
+  verificar adjacentes as trades (+1 por cada inimigo adjacente)*/
+countPoints(Board,[[R|[C|[]]]|Lb], AccPoints, FinalPoints, AccB, AccR) :-   getBoardCell(Board,R,C,[SystemID|[DominionID|_]]),
+																			getTradePoints(Board,R,C,DominionID,1,AccPoints,AccPoints1),
+																			getSystemTypePoints(SystemID,P,AccB,AccR,NewAccB,NewAccR), 
+																			NewPoints is AccPoints1 + P , 
 																			countPoints(Board,Lb,NewPoints,FinalPoints, NewAccB, NewAccR) .
+
+getTradePointsAux(Board,R,C,Acc,Team,Points,NewPoints) :- 	Team == 1,
+															getCellDirection(Board,R,C,Acc,Rf,Cf), 
+															getBoardCell(Board,Rf,Cf,[_|[2|_]]), !,
+															NewPoints is Points + 1 .
+getTradePointsAux(Board,R,C,Acc,Team,Points,NewPoints) :- 	Team == 2,
+															getCellDirection(Board,R,C,Acc,Rf,Cf), 
+															getBoardCell(Board,Rf,Cf,[_|[1|_]]), !,
+															NewPoints is Points + 1 .															
+getTradePointsAux(_,_,_,_,_,Points,Points).
 																			
-getTradePoints(Board,R,C,Team,Acc,Points) :- Team == 1, Acc > 0, Acc < 7, 
-											getCellDirection(Board,R,C,Acc,_,_,[_|[2|_]]), 
-											NewPoints is Points + 1,
-											NewAcc is Acc + 1,
-											getTradePoints(Board,R,C,Team,NewAcc,NewPoints) .
-											
-getTradePoints(Board,R,C,Team,Acc,Points) :- Team == 2, Acc > 0, Acc < 7, 
-											getCellDirection(Board,R,C,Acc,_,_,[_|[1|_]]), 
-											NewPoints is Points + 1,
-											NewAcc is Acc + 1,
-											getTradePoints(Board,R,C,Team,NewAcc,NewPoints) .
-getTradePoints(_,_,_,_,_,Points) .
+getTradePoints(Board,R,C,DominionID,Acc,Points,FinalPoints) :- 	dominion(DominionID,Team,'T'), Acc > 0, Acc < 7, 
+																getTradePointsAux(Board,R,C,Acc,Team,Points,NewPoints),
+																NewAcc is Acc + 1, 
+																getTradePoints(Board,R,C,DominionID,NewAcc,NewPoints,FinalPoints) .
+
+getTradePoints(_,_,_,_,_,Points,Points) .
 																			
 getSystemTypePoints(ID,P, AccB,AccR, AccB,AccR) :- (ID == 1 ; ID == 2; ID == 3), systemType(ID,_,P).
 getSystemTypePoints(4,0,AccB,AccR,AccB,NewAccR) :- (NewAccR is AccR + 1) .
 getSystemTypePoints(5,0,AccB,AccR,NewAccB,AccR) :- (NewAccB is AccB + 1) .
-getSystemTypePoints(ID,0,AccB,AccR,AccB,AccR) .
+getSystemTypePoints(_,0,AccB,AccR,AccB,AccR) .
 
 getNebulaePoints(Points, 0, Points).
 getNebulaePoints(Points, 1, NewPoints) :- NewPoints is Points + 1 .
@@ -92,7 +97,7 @@ biggestTerritoryPoints(Length1, Points1, Points1, Length2, Points2, Points2) :- 
 																					
 chooseWinner(_, Points, _, Points)	:-	nl,write('DRAW!').	
 chooseWinner(P1, Points1,_, Points2) :-   Points1 > Points2, displayWinner(P1, Points1).
-chooseWinner(_, Points1, P2, Points2) :-   Points1 < Points2, displayWinner(P2, Points2).
+chooseWinner(_, Points1, P2, Points2) :-   	Points1 < Points2, displayWinner(P2, Points2).
 
 displayWinner(Player, Points) :-	nl, write('THE WINNER IS - '), displayTeamName(Player), 
 									write(' WITH '), write(Points), write(' POINTS!'),nl .

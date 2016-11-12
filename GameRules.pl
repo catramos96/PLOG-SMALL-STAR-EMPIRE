40f2b8222edd1a12c5e40d39f09b40d0329e8dbe
Ines <gomes.inesisabel@gmail.com>
2016-11-12 21:06:37 +0000

    Funções de preparação para o bot 2 terminadas.

--- GameRules.pl
+++ GameRules.pl
@@ -11,7 +11,48 @@ getPossibleMoves(Board,Player,M) :- playerGetShips(Player,Ships), playerGetTeam(
 possibleMovesAux(Board,Team,[S|Sn],T,M) :- 	getPosition(S,R,C),nl,getAdjFreeCells(Board,R,C,Team,Cells),
 											append(T,[Cells|[]],T1), possibleMovesAux(Board,Team,Sn,T1,M).
 possibleMovesAux(_,_,_,M,M).
-									
+
+
+/* ------------------------- */									
+getPossibleMovesValued(Board,Player,ValuedList) :- 	getPossibleMoves(Board,Player,AllMoves),
+													setValuedMoves(Board,AllMoves,[],ValuedList).
+
+setValuedMoves(_,[],FinalList,FinalList).									
+setValuedMoves(Board,[Row|Remainder],List,FinalList) :- setValuedMovesByRow(Board,Row,[],RowList),
+														append(List,[RowList|[]],NewList),
+														setValuedMoves(Board,Remainder,NewList,FinalList).
+
+setValuedMovesByRow(_,[],FinalList,FinalList).
+setValuedMovesByRow(Board,[[R|[C|[]]]|Remainder],List,FinalList) :- getBoardCell(Board,R,C,[SystemType|_]),
+																	getValue(SystemType,Value),
+																	append(List,[Value|[]],NewList),
+																	setValuedMovesByRow(Board,Remainder,NewList,FinalList).
+												
+getValue(SystemType,SystemType) :- SystemType == 1 ; SystemType == 2 ; SystemType == 3 .
+getValue(SystemType,4) :- (SystemType == 4 ; SystemType == 5).								%TEMPORARIO
+getValue(_,0).
+
+/* Ship e Pos correspondem a row e col a procurar no AllMoves */
+getBestMove(Board,Player,Ship,Pos) :- 	getPossibleMovesValued(Board,Player,ValuedList),
+										searchBestMove(ValuedList,ValuedList,1,1,1,Ship,Pos).
+
+searchBestMove(_,[],_,BestR,BestC,BestR,BestC) .
+searchBestMove(ValuedList,[Row|Remainder],Ri,Rt,Ct,BestR,BestC) :- 	searchBestMoveByShip(ValuedList,Row,Ri,1,Rt,Ct,PartR,PartC),
+																	NewRi is Ri + 1,
+																	searchBestMove(ValuedList,Remainder,NewRi,PartR,PartC,BestR,BestC).
+
+searchBestMoveByShip(_,[],_,_,BestR,BestC,BestR,BestC) .																
+searchBestMoveByShip(ValuedList,[Value|Remainder],Ri,Ci,Rt,Ct,BestR,BestC) :- 	getPositionIn(Rt,Ct,ValuedList,Value2),
+																				Value > Value2,
+																				NewC is Ci + 1,
+																				searchBestMoveByShip(ValuedList,Remainder,Ri,NewC,Ri,Ci,BestR,BestC) .																	
+searchBestMoveByShip(ValuedList,[_|Remainder],Ri,Ci,Rt,Ct,BestR,BestC) :- 	NewC is Ci + 1,  
+																			searchBestMoveByShip(ValuedList,Remainder,Ri,NewC,Rt,Ct,BestR,BestC).
+
+getPositionIn(R,C,List,Value) :- nth1(R,List,Row), nth1(C,Row,Value).
+
+															
+/* ------------------------- */									
 
 															
 validMove(M,Pi,Ri,Ci,Rf,Cf) :- 	playerGetShips(Pi,Ships),				/*Get Ships*/

dadebdf25e898601d44001494aae71120772dd7a
catramos96 <catramos96@gmail.com>
2016-11-13 17:23:52 +0000

    Organização e Documentação do Player

--- Player.pl
+++ Player.pl
@@ -2,56 +2,73 @@
 *		PLAYER			*
 ************************/
 			
-player1 :- 	[1,0,[],[],[]].		
+player1 :- 	[1,0,[],[],[]].		/*[Team,Type,TradesPositions,ColoniesPositions,ShipsPositions]*/
 player2 :-	[2,0,[],[],[]].
 
-%CREATE
+/*
+createPlayer
+creates a player with Team and NShips in the position Base
+*/
 createPlayer(Team,NShips,Base,P) :-	playerAddBaseShips([Team,' ',[],[],[]],NShips,Base,P).
 										
-playerAddBaseShips(Pi,NShips,Base,Pf) :- 	NShips \= 0,
-											playerAddShip(Pi,Base,T), N1 is NShips-1,
-											playerAddBaseShips(T,N1,Base,Pf).
+playerAddBaseShips(PlayerI,NShips,Base,PlayerF) :- 	NShips \= 0,
+													playerAddShip(PlayerI,Base,PlayerT), N1 is NShips-1,
+													playerAddBaseShips(PlayerT,N1,Base,PlayerF).
 
-playerAddBaseShips(Pi,_,_,Pi).
+playerAddBaseShips(PlayerI,_,_,PlayerI).
 	
-%GETS
+/*
+Get Player's Information
+*/
 playerTeamName(P,'BLUE TEAM') :- 	playerGetTeam(P,T), T is 1.
 
 playerTeamName(P,'RED TEAM') :-	playerGetTeam(P,T), T is 2.	
 					
-playerGetTeam(Player,T) :-		getListElem(Player,1,T).
-playerGetType(Player,T) :-		getListElem(Player,2,T).
-playerGetTrades(Player,T) :- 	getListElem(Player,3,T).
-playerGetColonies(Player,C) :- 	getListElem(Player,4,C).
-playerGetShips(Player,S) :- 	getListElem(Player,5,S).
+playerGetTeam(Player,Team) :-		getListElem(Player,1,Team).
+playerGetType(Player,Type) :-		getListElem(Player,2,Type).
+playerGetTrades(Player,Trades) :- 	getListElem(Player,3,Trades).
+playerGetColonies(Player,Colonies) :- 	getListElem(Player,4,Colonies).
+playerGetShips(Player,Ships) :- 	getListElem(Player,5,Ships).
 
-getPosition([R|[C|[]]],R,C).
+getPosition([Row|[Column|[]]],Row,Column).
 playerTerritory(Player, List) :- getListElem(Player,3,Trade), getListElem(Player,4,Colony), append(Colony, Trade, List) .	
 
-%SETS
-playerSetShip(Pi,SPosi,SPosf,Pf) :-	playerRemShip(Pi,SPosi,Pt) , playerAddShip(Pt,SPosf,Pf).
+/*
+Sets Player's Information
+*/
+playerSetShip(PlayerI,ShipPosI,ShipPosF,PlayerF) :-	playerRemShip(PlayerI,ShipPosI,Pt) , playerAddShip(Pt,ShipPosF,PlayerF).
 playerSetType(PlayerI,Type,PlayerF) :- setList(Type,PlayerI,2,PlayerF).
 
-%ADDS
-playerAddControl(Pi,Type,Tpos,Pf) :-	Type == 'C', !,							
-										playerAddColony(Pi,Tpos,Pf).										
-playerAddControl(Pi,Type,Tpos,Pf) :-	Type == 'T', !,
-										playerAddTrade(Pi,Tpos,Pf).
-playerAddControl(Pi,_,_,Pi).																
+/*
+Ads Player's positions
+*/
+playerAddControl(PlayerI,Type,Position,PlayerF) :-	Type == 'C', !,							
+										playerAddColony(PlayerI,Position,PlayerF).										
+playerAddControl(PlayerI,Type,Position,PlayerF) :-	Type == 'T', !,
+										playerAddTrade(PlayerI,Position,PlayerF).
+playerAddControl(PlayerI,_,_,PlayerI).																
 										
-playerAddTrade(Pi,TPos,Pf) :- 	getListElem(Pi,3,Tr), addList(TPos,Tr,Trf), setList(Trf,Pi,3,Pf).	
-playerAddColony(Pi,CPos,Pf) :- 	getListElem(Pi,4,C), addList(CPos,C,Cf), setList(Cf,Pi,4,Pf).
-playerAddShip(Pi,SPos,Pf) :- 	getListElem(Pi,5,S), addList(SPos,S,Sf), setList(Sf,Pi,5,Pf).
+playerAddTrade(PlayerI,Position,PlayerF) :- 	getListElem(PlayerI,3,Tr), addList(Position,Tr,Trf), setList(Trf,PlayerI,3,PlayerF).	
+playerAddColony(PlayerI,Position,PlayerF) :- 	getListElem(PlayerI,4,C), addList(Position,C,Cf), setList(Cf,PlayerI,4,PlayerF).
+playerAddShip(PlayerI,Position,PlayerF) :- 	getListElem(PlayerI,5,S), addList(Position,S,Sf), setList(Sf,PlayerI,5,PlayerF).
 
-%REMOVE
-playerRemShip(Pi,SPos,Pf) :-	playerGetShips(Pi,S), remList(SPos,S,Sf), setList(Sf,Pi,5,Pf).	
+/*
+playerRemShip
+remove a ship with Position of the shipsPositions in the player
+*/
+playerRemShip(PlayerI,Position,PlayerF) :-	playerGetShips(PlayerI,Ships), remList(Position,Ships,ShipsF), setList(ShipsF,PlayerI,5,PlayerF).	
 
-%CONFIRMATION
-hasShip(Pi,Row,Column) :- 	playerGetShips(Pi,S),
-								member([Row|[Column|[]]],S).
+/*
+hasShip
+true if the player has a ship with the position [Row,Column]
+*/
+hasShip(PlayerI,Row,Column) :- 	playerGetShips(PlayerI,Ships),
+								member([Row|[Column|[]]],Ships).
 
-%DISPLAYS
-
+/*
+displayPlayerInfo
+displays the information about the player
+*/
 displayPlayerInfo(Player) :- 	write('TRADES: '),nl, playerGetTrades(Player,Trades), displayList(Trades), nl, nl,
 								write('COLONIES: '), nl ,playerGetColonies(Player,Colonies),displayList(Colonies), nl, nl,
 								write('SHIPS: '), nl, playerGetShips(Player,Ships), displayList(Ships),nl, nl.
